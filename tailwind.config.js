506c83a90651467f2e3ac44e4eb38f765c81094e
sphnikola <sphnikola@gmail.com>
2024-02-16 18:52:56 +0000

    Add spacing and font styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,13 @@ export default {
       screens: {
         md: "640px",
         lg: "875px",
+        xl: "1400px",
       },
       colors: {
         "nav-link": "rgb(146, 155, 170)",
         "button-bg": "rgb(252, 183, 44)",
+        "p-color": "rgb(146,155,170)",
+        "h4-color": "rgb(74,86,104)",
       },
       backgroundImage: {
         home_hero_mobile: "url('./src/assets/images/home-hero-mobile.jpg')",
