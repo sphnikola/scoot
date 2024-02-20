73271d89f4b724f0278341ff830a8ccc5d3685a7
sphnikola <sphnikola@gmail.com>
2024-02-20 12:17:55 +0000

    Add footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,44 @@
 import google_play from "../assets/icons/google-play.svg";
 import app_store from "../assets/icons/app-store.svg";
+import fb from "../assets/icons/facebook.svg";
+import ig from "../assets/icons/instagram.svg";
+import x from "../assets/icons/twitter.svg";
+import logo from "../assets/logo-white.svg";
 export function Footer() {
   return (
     <>
-      <footer className="footer relative">
-        <section className="footer_1st bg-footer1st-bg py-[90px] px-[24px]">
-          <div className="flex flex-col items-center gap-y-8">
+      <footer className="footer overflow-hidden">
+        <section className="relative bg-footer1st-bg py-[90px] px-[24px]">
+          <div className="footer_1st flex flex-col items-center gap-y-8 lg:flex-row lg:justify-center lg:gap-x-14">
             <h3>Sign up and Scoot off today</h3>
             <div className="flex gap-x-5">
-              <button>
+              <a href="#">
                 <div>
                   <img src={app_store} />
                 </div>
-              </button>
-              <button>
-                <div>
+              </a>
+              <form>
+                <button type="submit">
                   <img src={google_play} />
-                </div>
-              </button>
+                </button>
+              </form>
+            </div>
+          </div>
+        </section>
+        <section className="footer_2nd bg-footer2nd-bg py-[25px] px-[33px] ">
+          <div className="flex justify-between ">
+            <nav className="flex gap-x-8 items-center">
+              <img src={logo} className="object-contain" />
+              <ul className="flex gap-x-3">
+                <li>About</li>
+                <li>Location</li>
+                <li>Services</li>
+              </ul>
+            </nav>
+            <div className="inline-flex gap-x-5">
+              <img src={fb} className="object-contain" />
+              <img src={x} className="object-contain" />
+              <img src={ig} className="object-contain" />
             </div>
           </div>
         </section>
