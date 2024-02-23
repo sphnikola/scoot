759ea381ac35bad54572e88358424957dfe8d25d
sphnikola <sphnikola@gmail.com>
2024-02-23 02:11:01 +0000

    Add hero section for about page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { MainSection } from "./components/MainSection";
 import { NavbarSimple } from "./components/NavbarSimple";
 import { Steps } from "./components/Steps";
 import { Telemetry } from "./components/Telemetry";
+import About from "./pages/About";
 import Home from "./pages/Home";
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
           <Steps />
           <Telemetry />
         </MainSection> */}
-        <Home />
+        <About />
+        {/* <Home /> */}
         <Footer />
       </div>
     </>
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <>
+      <section className="about_hero relative py-[50px] px-[26px]">
+        <div>
+          <h2 className="text-center md:text-left">About</h2>
+        </div>
+      </section>
+    </>
+  );
+}
