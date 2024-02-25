88082773f7fd77af8e1cd8e975f820d64bdf3144
sphnikola <sphnikola@gmail.com>
2024-02-25 03:44:01 +0000

    update sections

--- src/components/AccordionCustomIcon.jsx
+++ src/components/AccordionCustomIcon.jsx
@@ -26,7 +26,7 @@ function Icon({ id, open }) {
   );
 }
 
-export function AccordionCustomIcon() {
+export function AccordionCustomIcon(props) {
   const [open, setOpen] = React.useState(0);
 
   const handleOpen = (value) => setOpen(open === value ? 0 : value);
@@ -39,36 +39,27 @@ export function AccordionCustomIcon() {
         icon={<Icon id={1} open={open} />}
       >
         <AccordionHeader onClick={() => handleOpen(1)}>
-          What is Material Tailwind?
+          {props.headerOne}
         </AccordionHeader>
-        <AccordionBody>
-          We&apos;re not always in the position that we want to be at.
-          We&apos;re constantly growing. We&apos;re constantly making mistakes.
-          We&apos;re constantly trying to express ourselves and actualize our
-          dreams.
-        </AccordionBody>
+        <AccordionBody>{props.bodyOne}</AccordionBody>
       </Accordion>
-      <Accordion open={open === 2} icon={<Icon id={2} open={open} />}>
+
+      <Accordion
+        className=" max-w-3xl"
+        open={open === 2}
+        icon={<Icon id={2} open={open} />}
+      >
         <AccordionHeader onClick={() => handleOpen(2)}>
-          How to use Material Tailwind?
+          {props.headerTwo}
         </AccordionHeader>
-        <AccordionBody>
-          We&apos;re not always in the position that we want to be at.
-          We&apos;re constantly growing. We&apos;re constantly making mistakes.
-          We&apos;re constantly trying to express ourselves and actualize our
-          dreams.
-        </AccordionBody>
+        <AccordionBody>{props.bodyTwo}</AccordionBody>
       </Accordion>
+
       <Accordion open={open === 3} icon={<Icon id={3} open={open} />}>
         <AccordionHeader onClick={() => handleOpen(3)}>
-          What can I do with Material Tailwind?
+          {props.headerThree}
         </AccordionHeader>
-        <AccordionBody>
-          We&apos;re not always in the position that we want to be at.
-          We&apos;re constantly growing. We&apos;re constantly making mistakes.
-          We&apos;re constantly trying to express ourselves and actualize our
-          dreams.
-        </AccordionBody>
+        <AccordionBody>{props.bodyThree}</AccordionBody>
       </Accordion>
     </>
   );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,8 @@
-import telemetry from "../assets/images/telemetry.jpg";
-import second_telemetry from "../assets/images/near-you.jpg";
+import digital_era from "../assets/images/digital-era.jpg";
+import better_living from "../assets/images/better-living.jpg";
 import tech from "../assets/images/our-tech.jpg";
+import intergrity from "../assets/images/our-integrity.jpg";
+import community from "../assets/images/our-community.jpg";
 import { AccordionCustomIcon } from "../components/AccordionCustomIcon";
 export default function About() {
   return (
@@ -11,16 +13,16 @@ export default function About() {
         </div>
       </section>
 
-      <section className="telemetry relative">
-        <div className="flex flex-col items-center text-center gap-y-8 px-6 lg:flex-row-reverse lg:text-left lg:px-20">
+      <section className="telemetry relative p-[8vw]">
+        <div className="flex flex-col items-center text-center gap-y-8  lg:flex-row-reverse lg:text-left ">
           <img
             className="rounded-full    object-contain lg:w-[400px]"
-            src={telemetry}
+            src={digital_era}
           />
 
           <div className="space-y-5">
             <h4>Easy to use riding telemetr</h4>
-            <p className="px-[30px] lg:pl-[0px] lg:pr-[10px]">
+            <p className="px-[30px] md:pl-[0px] ">
               The Scoot app is available with riding telemetry. This means it
               can show you your average speed, how long you've been using the
               scooter, your traveling distance, and many more things all in an
@@ -31,15 +33,15 @@ export default function About() {
         </div>
       </section>
 
-      <section className="second-telemetry relative mt-[60px]">
-        <div className="flex flex-col items-center text-center gap-y-8 px-6 lg:flex-row lg:text-left lg:px-20">
+      <section className="second-telemetry relative mt-[60px] lg:text-left  p-[8vw]">
+        <div className="flex flex-col items-center text-center gap-y-8 lg:flex-row lg:text-left">
           <img
             className="rounded-full lg:w-[400px] object-contain "
-            src={second_telemetry}
+            src={better_living}
           />
-          <div className="space-y-5 lg:px-7">
+          <div className="space-y-5 lg:pl-7 lg:text-right">
             <h4>Easy to use riding telemetr</h4>
-            <p className="  ">
+            <p className=" px-[30] lg:pl-[30px] ">
               The Scoot app is available with riding telemetry. This means it
               can show you your average speed, how long you've been using the
               scooter, your traveling distance, and many more things all in an
@@ -50,7 +52,7 @@ export default function About() {
         </div>
       </section>
 
-      <section>
+      <section className="p-[8vw]">
         <h3 className="text-center">Our Values</h3>
         <div className="lg:flex ">
           <div className="flex flex-col items-center md:flex-row md:justify-between lg:flex-col">
@@ -71,7 +73,7 @@ export default function About() {
           </div>
           <div className="flex flex-col items-center md:flex-row md:justify-between lg:flex-col">
             <div className="relative mb-14 md:pr-10  md:border-r-[1px] md:border-r-[#A9A8C6] lg:pr-0 lg:border-none">
-              <img className="rounded-full " src={tech} />
+              <img className="rounded-full " src={intergrity} />
               <div className=" bg-[#fcb72c] h-[100px] w-[100px] rounded-full absolute top-[170px] left-[70px] z-10">
                 <h5 className="text-center pt-[36px]">01</h5>
               </div>
@@ -87,7 +89,7 @@ export default function About() {
           </div>
           <div className="flex flex-col items-center md:flex-row md:justify-between lg:flex-col">
             <div className="relative mb-14 md:pr-10  md:border-r-[1px] md:border-r-[#A9A8C6] lg:pr-0 lg:border-none">
-              <img className="rounded-full " src={tech} />
+              <img className="rounded-full " src={community} />
               <div className=" bg-[#fcb72c] h-[100px] w-[100px] rounded-full absolute top-[170px] left-[70px] z-10">
                 <h5 className="text-center pt-[36px]">01</h5>
               </div>
@@ -104,20 +106,25 @@ export default function About() {
         </div>
       </section>
 
-      <section className="faqs">
+      <section className="faqs p-[8vw]">
         <div>
-          <h4>FAQS</h4>
-          <div className=" mb-10 flex flex-col items-center lg:flex-row lg:gap-x-28">
+          <h4 className="text-center">FAQS</h4>
+          <div className=" mb-10 flex flex-col items-center lg:flex-row lg:gap-x-28 lg:justify-center">
             <h4 className="">How it works</h4>
             <div className="">
-              <AccordionCustomIcon />
+              <AccordionCustomIcon
+                headerOne="How do i download the app"
+                bodyOne="To download the Scoot app, you can search “Scoot” in both the App and Google Play stores. An even simpler way to do it would be to click the relevant link at the bottom of this page and you’ll be re-directed to the correct page. "
+                headerTwo="can i find nearby scoots"
+                bodyTwo=" Definitely! Simply open up the app and allow us to find your location while using it. We'll show you all of the closest Scoots and some extra useful information. "
+                headerThree="do i need a lincense plate"
+                bodyThree="Yup! We provide information inside the app regarding local laws and the license you need to be able to ride our Scoots. "
+              />
             </div>
           </div>
           <div className="flex flex-col items-center lg:flex-row lg:gap-x-28">
             <h4 className="">Safe Driving</h4>
-            <div className="">
-              <AccordionCustomIcon />
-            </div>
+            <div className="">{/* <AccordionCustomIcon /> */}</div>
           </div>
         </div>
       </section>
