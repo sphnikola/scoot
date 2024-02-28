a51f126375eff26d85a60642b5ff8e76c647f331
sphnikola <sphnikola@gmail.com>
2024-02-28 03:51:32 +0000

    Removes unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import logo from "./assets/logo.svg";
 import { Footer } from "./components/Footer";
-import { Hero } from "./components/Hero";
-import { MainSection } from "./components/MainSection";
 import { NavbarSimple } from "./components/NavbarSimple";
-import { Steps } from "./components/Steps";
-import { Telemetry } from "./components/Telemetry";
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Careers from "./pages/Careers";
-import Home from "./pages/Home";
 function App() {
   return (
     <>
       <div className=" max-w-max mx-auto overflow-hidden">
         <NavbarSimple />
-        {/* <Hero />
-        <MainSection>
-          <Steps />
-          <Telemetry />
-        </MainSection> */}
         <About />
         {/* <Careers /> */}
         {/* <Home /> */}
