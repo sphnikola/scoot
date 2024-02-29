aa5d47575b2bfcd00afacbd43cea5c7b4648a5c0
sphnikola <sphnikola@gmail.com>
2024-02-29 01:01:34 +0000

    Add Location page and hero section for the page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { NavbarSimple } from "./components/NavbarSimple";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Careers from "./pages/Careers";
+import { Location } from "./pages/Location";
 function App() {
   return (
     <>
       <div className=" max-w-max mx-auto overflow-hidden">
         <NavbarSimple />
-        <About />
+        {/* <About /> */}
+        <Location />
         {/* <Careers /> */}
         {/* <Home /> */}
         <Footer />
---
+++ src/pages/Location.jsx
@@ -0,0 +1,13 @@
+export function Location() {
+  return (
+    <>
+      <section className="about_hero relative py-[50px] px-[8vw]">
+        <div>
+          <h2 className="text-center text-white text-[43.2px] leading-[43.2px] tracking-[2px] md:text-left md:text-[48px] md:tracking-normal  space-mono-bold">
+            Location
+          </h2>
+        </div>
+      </section>
+    </>
+  );
+}
